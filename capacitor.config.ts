a69b6d3cedabdc6fa7868cae8a58e78b4c30d235
Trần Anh Tú <tutran1998.tt@gmail.com>
2023-07-21 09:57:52 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-driver-management-system',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
