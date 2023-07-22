7aef5cd6073f3a3e15870907be23947c097bed0f
Trần Anh Tú <tutran1998.tt@gmail.com>
2023-07-22 10:18:44 +0700

    Add GoogleService
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.msc.driver',
   appName: 'app-driver-management-system',
+  // bundledWebRuntime: false,
   webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  },
   server: {
     androidScheme: 'https'
   }
